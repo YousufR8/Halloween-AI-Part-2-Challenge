711335f8cc9ce2294df1daf23e99c25960c44f84
YousufR8 <Yrehman06@gmail.com>
2023-11-14 12:18:26 -0500

    Add project files.

---
+++ Halloween AI Part 2 Challenge/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace ScaryHalloweenStoryApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCreateStoryButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Combine user input to create the complete scary Halloween story
+            string story = $"{part1TextBox.Text}\n{part2TextBox.Text}\n{part3TextBox.Text}";
+            // Add more parts as needed
+
+            // Display the complete story in the TextBlock
+            storyTextBlock.Text = story;
+        }
+    }
+}
